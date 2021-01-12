243e778c147ee962769c9ab1e42fc810aef0df29
David <dhutchis68516@gmail.com>
2021-01-12 12:11:58 -0600

    add new hobby form

--- hobbie-collections-frontend/src/index.js
+++ hobbie-collections-frontend/src/index.js
@@ -8,15 +8,34 @@ const init = () => {
 
 function addEventListeners() {
     document.getElementById('hobbies').addEventListener('click', renderHobbies)
+    document.getElementById('hobby-form').addEventListener('click', displayCreateHobbyForm)
 }
 
 async function renderHobbies() {
-    const hobb = await apiService.fetchHobbies()
     main.innerHTML = ''
+    const hobb = await apiService.fetchHobbies()
     hobb.map(h => {
         const newHobby = new Hobby(h) 
         main.innerHTML += newHobby.renderHobbies()
     })
+}
+
+function displayCreateHobbyForm() {
+    main.innerHTML = ""
+    let formDiv = document.querySelector("#new-hobby-form")
+    let html = `
+    <form>
+        <label>Create New Hobby Type:</label>
+        <input type="text" id="name"><br><br>
+        <input type="sumbit">
+    </form
+    `
+    formDiv.innerHTML = html
+    document.querySelector('form').addEventListener('submit', createHobby)
+}
+
+function createHobby() {
+
 }
 
 init()
